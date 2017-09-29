9d5f5e5f1f6b9049f12e2a1f697da06e4b993e95
kenneth558 <kenlovesjesus@gmail.com>
2017-09-29 16:22:30 -0500

    Update structs.h

--- structs.h
+++ structs.h
@@ -43,7 +43,7 @@ volatile typedef struct port_specific
 volatile typedef struct one_line_device_protocols_supported //Make array of these different types of device protocols, this holds specifics
 {
     u8 millis_oldDHT_MCU_start_signal_bit;
-    unsigned short millis_rest_length;
+    unsigned short millis_rest_length;//makes for slightly faster access inside the ISR
     unsigned short micros_data_acq_time_max;
     //0 = no device attached, 255 = yet to be identified, 1-254 indicate various protocol-rest period combinations with 1-3 being generic DHT protocol,  rest period 1 sec, 2 sec, or (3) TBD
     u8 dht_max_transitions_for_valid_acquisition_stream;
@@ -61,14 +61,9 @@ volatile typedef struct device_specific
     volatile u8 index_of_next_valid_readings_sets;
     volatile unsigned long timestamp_of_pin_valid_data_millis = 0;
     volatile unsigned long timestamp_of_pin_last_attempted_device_read_millis = 0;
-//    u16 low_going_interval_micros = 0;
-    volatile unsigned long high_going_timestamp_micros = 0;                                                     //Don't corrupt this for error-communications.  The last one from device is needed to set the rest-time timer much later
-    volatile unsigned long device_busy_resting_until_this_system_millis;
+    volatile unsigned long device_busy_resting_this_more_millis;
     volatile unsigned long start_time_plus_max_acq_time_in_uSecs;//This will get null'd when data is done processing
-    volatile bool millis_will_overflow;
-    volatile bool micros_will_overflow;
     volatile u8 devprot_index;//to a Device Protocols array element
-    volatile u8 portspec_index_for_pin;
     volatile u8 consecutive_read_failures;
     volatile u8 consecutive_read_successes;
     volatile byte mask_in_port;
@@ -115,32 +110,23 @@ struct ISR_specific
     volatile u8* active_pin_pin_reg_addr;//This pointer is the address of a portInputRegister of the PORT
     volatile byte mask_by_port_of_current_device_being_actively_communicated_with_thisISR = 0;//will ever only have single bit set at any time to AND against the port's pin register contents, so each bit not guarranteed to have a unique target device.  Use in combo with active_pin_pin_reg_addr
     volatile byte mask_by_PCMSK_of_current_device_within_ISR = 0;
-    volatile bool* micros_will_overflow;
-    volatile bool* millis_will_overflow;
     volatile unsigned long* start_time_plus_max_acq_time_in_uSecs;//for DHT the max acq time is 4416 uSecs for 40 data bits.   Calculated from the max number of one bits is 25 of the 40 bits plus add 4 uSec for resolution err
-    volatile unsigned long* high_going_timestamp_micros  = 0;                                                     //Don't corrupt this for error-communications.  The last one from device is needed to set the rest-time timer much later
     volatile u8 next_bit_coming_from_dht = 0;//set to -1 (255) if acquisition complete? later
-    volatile unsigned long timestamps[ ( ( dht_max_transitions_for_valid_acquisition_stream ) * 2 ) + 10 ]; //4 bytes per element x 3 ISRs x 43 
-//    volatile unsigned long timestamps[dht_max_transitions_for_valid_acquisition_stream - 1]; //4 bytes per element x 3 ISRs x 43 
-//    volatile u8 pin_numbers_in_PCMSK[8];
+    volatile unsigned long timestamps[ dht_max_transitions_for_valid_acquisition_stream + 1 ]; //4 bytes per element x 3 ISRs x 43 
     volatile byte mask_by_PCMSK_of_real_pins = 0;
-    volatile byte mask_of_resting_devices_pins = 0;
     volatile byte mask_by_PCMSK_of_valid_devices = 0; //used to know indexes within run-time-sized array of devices: this is to x-ref to devices array. also used to determine size of array of device characteristics
     volatile u8* pcmsk;
     volatile unsigned short millis_rest_length;
-    volatile u8 array_of_all_pinnums_plus_one_this_ISR[ 8 ];//may not need all three of these in final version, never to be sparsed, no savings can be had by sparsing
     volatile u8 array_of_all_devprot_index_this_ISR[ 8 ];//This can only be determined ultimately from port in device discovery -> port mask -> 
     volatile unsigned short* val_tmp1;// = ( unsigned short* )&this_Isrspec_address->sandbox_bytes[ 1 ];
     volatile unsigned short* val_tmp2;// = ( unsigned short* )&this_Isrspec_address->sandbox_bytes[ 3 ];
-//    volatile float val_tmp; //With float, the max time seen in isr is 220 uSec
-//    volatile float* val_tmp; //With float*, the max time seen in isr is   uSec
     volatile u8 pwroftwo;
     volatile u8 interval = 2;
     volatile signed char offset = 0;
 };
 
 //There is one non-struct dynamic-sized array for each ISR, each element is a device-specific struct on one of the ISR's populated PCMSK bits
-
+/*
 volatile typedef struct pin_specific
 {
     u8 Dpin;
@@ -158,7 +144,7 @@ volatile typedef struct registers_etc_this_dht_populated_pin_no_ISR
 {
     PINSPEC* Dpin;
 } DHT_NO_ISR;
-
+*/
 volatile typedef struct ISR_pin_registers_xref_etc_unsparsed                                            // This is needed to provide which index the interrupt-generating pin is in the jam-packed non-sparse array (only pins with dht devices are members, zero-based, no holes)
 {                                                                                                       // One per dht-populated ISR
     byte index_lookup_table[ 8 ];                                                                         // Requires minimum of 24 bits for 8 elements of minimum 3 bit size. Each element represents a pin from the PCMSK of this ISR, with same order as in PCMSK, naturally.
@@ -171,8 +157,6 @@ volatile typedef struct D_Timer //piggy-backed to Timer0 match A.  Used for timi
     volatile u8 on_hold_until_called_by_another_process = 0; //a number between 0 and 255 indicates a valid index+1 of the other process that will start this later. 0 indicates timer is available for use, 255 indicates timer in use solo
     volatile unsigned long timeset_millis = 0;
 //    bool ( *function )( u8 ); //Makes this somewhat re-purposeable
-    volatile bool millis_will_overflow;
-    volatile bool micros_will_overflow;
     volatile u8 which_ISR;//which_ISR IS AFTER XREF, NOT NEEDING TO GET XREF'D AGAIN
 } Device_Timer;
 /*
